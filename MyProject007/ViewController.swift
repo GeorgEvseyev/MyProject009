5c8b93b7a53f349636c9df9c3e525901dbf71bae
GeorgEvseyev <georgevseyev@icloud.com>
2022-06-20 10:34:52 +0300

    Initial Commit

---
+++ MyProject007/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProject007
+//
+//  Created by Георгий Евсеев on 20.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
